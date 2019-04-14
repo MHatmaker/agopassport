12bdf9da612dda7dcf75a27efa7d36d250fcde7e
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2019-04-14 15:05:42 -0700

    auth fiddling in url 2

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var cbport = process.env.PORT || "3000";
 // var hurl = encodeURIComponent("https://agopassport.herokuapp.com:" + process.env.PORT || "3000" + "/auth/arcgis/callback");
 // var hurl = encodeURIComponent("/auth/" + cbport + "arcgis/callback");
 // var hurl = "https://agopassport.herokuapp.com:" + cbport + "/auth/arcgis/callback";
-var hurl = ":" + cbport + "/arcgis/callback";
+var hurl = "/arcgis/callback" + ":" + cbport ;
 
 console.log("hurl");
 console.log(hurl);
