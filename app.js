0f79c5151bff9259cd29fc1e40e08dc084749909
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2019-04-14 16:27:07 -0700

    retries full heroku path in Strategy

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ var agotoken;
 var cbport = process.env.PORT || "3000";
 // var hurl = encodeURIComponent("https://agopassport.herokuapp.com:" + process.env.PORT || "3000" + "/auth/arcgis/callback");
 // var hurl = encodeURIComponent("/auth/" + cbport + "arcgis/callback");
-// var hurl = "https://agopassport.herokuapp.com:" + cbport + "/auth/arcgis/callback";
+var hurl = "https://agopassport.herokuapp.com:" + cbport + "/auth/arcgis/callback";
 // var hurl = ":" + cbport + "/arcgis/callback";
-var hurl = "/auth/arcgis/callback";
+// var hurl = "/auth/arcgis/callback";
 
 console.log("hurl");
 console.log(hurl);
